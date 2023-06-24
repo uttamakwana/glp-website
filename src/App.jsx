5f3ddec854aa33155e1074d62325f66a9167ae92
Uttam Makwana <uttamakwana4503@gmail.com>
2023-06-24 17:03:56 +0530

    Created a Home page and added Google Fonts and also created a HomeProvider for all kinds of State Management in Home will be held there!

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <div className="App">
+      {/* Home page where all the Components like 
+        -> Header, Footer, Feeds, College Details
+      */}
       <Home />
+      {/* Auth page, where all the Login, Register stuff will be held */}
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// importing Header Component from Componenet folder where all exports of Components folder like Header is defined in index.js file
 import { Header } from "../../components";
 
 const Home = () => {
