32db67599a8d51695a4155ff1d197b7e1ec7c76f
Uttam Makwana <uttamakwana4503@gmail.com>
2023-06-24 14:03:02 +0530

    Created a Page folder where all the routes will be defined and Just getting started with Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-
   return (
     <div className="App">
-
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
