0c9d9f4e1cf32a7f798773dc0628ee406ed7b633
Uttam Makwana <uttamakwana4503@gmail.com>
2023-06-25 10:46:25 +0530

    Commented the Header component very well

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,24 +8,34 @@ import { TfiMenu } from "react-icons/tfi";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const Header = () => {
+  // toggleSearch will toggle the search bar in header
   const [toggleSearch, setToggleSearch] = useState(false);
+  // toggleMenu will toggle the menu bar for mobile display
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     // Header componenet starts here
     <header className="header">
+      {/* header logo container */}
       <nav className="header__logo-container">
+        {/* main logo for header */}
         <figure>
           <figcaption className="heading-text">Logo</figcaption>
         </figure>
       </nav>
+      {/* header navigation container */}
       <nav className="header__navgiation-container">
+        {/* header navigation list */}
         <ul
           className={`header__navigation-container__navigation ${
             toggleMenu ? "active-menu" : ""
           } `}
         >
-          <li id="search-bar">
+          {/* header navigation list item */}
+          <li
+            id="search-bar"
+            className="header__navigation-container__navigation_item"
+          >
             <input
               type="text"
               name="search"
@@ -49,19 +59,26 @@ const Header = () => {
               }}
             />
           </li>
-          <li>
+          {/* header navigation list item */}
+
+          <li className="header__navigation-container__navigation_item">
             <LuMessageCircle />
             {toggleMenu ? "Messages" : ""}
           </li>
-          <li>
+          {/* header navigation list item */}
+
+          <li className="header__navigation-container__navigation_item">
             <MdOutlineNotificationsActive />
             {toggleMenu ? "Notifications" : ""}
           </li>
-          <li>
+          {/* header navigation list item */}
+
+          <li className="header__navigation-container__navigation_item">
             <CgProfile />
             {toggleMenu ? "Profile" : ""}
           </li>
 
+          {/* close icon for mobile display */}
           {toggleMenu ? (
             <AiOutlineCloseCircle
               id="close"
@@ -74,6 +91,7 @@ const Header = () => {
           )}
         </ul>
 
+        {/* header toggle menu bar icon for mobile/tablet display */}
         <div className="toggle-bar-container">
           <TfiMenu
             onClick={() => {
